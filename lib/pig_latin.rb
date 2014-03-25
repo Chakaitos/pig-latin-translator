899f66403668dd08d2bd8564d3d81cdcf47457b5
Jose R. Borja <chakaitos@gmail.com>
2014-03-25 17:24:44 -0500

    Fix tests and and implement the translate method - all tests pass

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,5 +1,17 @@
-require "pig_latin/version"
+# require "pig_latin/version"
 
 module PigLatin
-  # Your code goes here...
+	
+	def self.translate(word)
+		pig_word = ''
+		if word.match(/^([y+])([a-z]*)/)
+		 	pig_word = $2 + $1 + "ay"
+		elsif word.match(/^[aeiou+]/)
+			pig_word = word + "way"
+		elsif word.match(/(^[^aeiouy]+)([a-z]*)/)
+		 	pig_word = $2 + $1 + "ay"
+		else
+			"That is not a word."
+		end
+	end
 end
